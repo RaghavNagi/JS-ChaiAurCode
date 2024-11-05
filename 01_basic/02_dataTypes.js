96cbb120d5d21c2b3b4dae37691c12e75612485a
RaghavNagi <raghavnagi81@gmail.com>
2024-11-05 10:14:59 +0530

    Adding documentation link

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -3,6 +3,43 @@
 // alert(3+3) //we are using node js not browser
 //alert function can be used in browser
 
+
+/*
+The alert function in JavaScript is used to display a simple,
+modal dialog box with a specified message and an “OK” button.
+This function is often used for debugging or to quickly show information to the user,
+though it’s generally not recommended for production use due to its blocking behavior and lack of styling flexibility.
+
+Basic Syntax
+
+alert("Your message here");
+
+Example Usage
+
+alert("Hello, World!");
+
+Important Notes
+
+	1.	Blocking Behavior: The alert function pauses JavaScript execution until the user closes the alert box.
+    This can interrupt user experience, especially if misused in loops or critical paths.
+	2.	No Customization: You cannot change the appearance or style of the alert box since it’s a browser-native dialog.
+	3.	Better Alternatives: For non-blocking notifications,
+    consider using libraries (like SweetAlert, Toastify) or custom modals created with HTML/CSS/JavaScript.
+
+Example in a Function
+
+function greetUser() {
+    alert("Welcome to our website!");
+}
+
+When this function is called, it will display an alert with the message, “Welcome to our website!”.
+*/
+
+// alert function documentation
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+
+
 console.log(3+3);
 
 let name = "Raghav";
