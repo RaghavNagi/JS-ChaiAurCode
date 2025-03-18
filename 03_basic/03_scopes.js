793ec6e96f2ad0bf65101068cf233e4d3c70e2fa
RaghavNagi <raghavnagi81@gmail.com>
2025-03-18 20:14:20 +0530

    Scope Level

---
+++ 03_basic/03_scopes.js
@@ -0,0 +1,21 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+
+
+
+var c = 300;
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+
+// console.log(a);  // it will give error
+// console.log(b);  // it will give error
+console.log(c);     // it will print its value - 30
+
+
