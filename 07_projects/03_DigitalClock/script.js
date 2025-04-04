e3e59332beb72e614dcb7cdf30627639f7ae2fef
RaghavNagi <raghavnagi81@gmail.com>
2025-04-04 09:08:42 +0530

    Digital clock

---
+++ 07_projects/03_DigitalClock/script.js
@@ -0,0 +1,11 @@
+// document.querySelector('#clock')
+// we can also use
+const clock = document.getElementById('clock')
+
+
+
+setInterval(function(){
+    let date = new Date()
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000)
